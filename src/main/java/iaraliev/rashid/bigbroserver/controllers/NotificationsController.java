35510d9b43ed62e390306fbd4645e695160f7a23
YRashid <ry1312@ya.ru>
2017-11-26 11:34:21 +0300

    notifications

--- src/main/java/iaraliev/rashid/bigbroserver/controllers/NotificationsController.java
+++ src/main/java/iaraliev/rashid/bigbroserver/controllers/NotificationsController.java
@@ -1,12 +1,9 @@
 package iaraliev.rashid.bigbroserver.controllers;
 
-import iaraliev.rashid.bigbroserver.model.domain.Car;
-import iaraliev.rashid.bigbroserver.model.entity.Camera;
 import iaraliev.rashid.bigbroserver.repository.CameraRepository;
 import iaraliev.rashid.bigbroserver.utils.PythonServerRequester;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/notifications")
 public class NotificationsController {
 
-    private final CameraRepository cameraRepository;
+    private volatile boolean doIt = false;
 
     @Autowired
     public NotificationsController(CameraRepository cameraRepository, PythonServerRequester pythonServerRequester) {
@@ -26,9 +23,20 @@ public class NotificationsController {
     public
     @ResponseBody
     String getNotification() {
-        String result = "12";
+        String result = null;
+        if (doIt) {
+            result = "Take your car away because there will be snow cleaning";
+            doIt = false;
+        }
         return result;
+    }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public
+    @ResponseBody
+    Boolean addNotifiaction() {
+        doIt = true;
+        return doIt;
     }
 
 
